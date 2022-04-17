69767aae8e5c71136ee207c6ceae820de705d912
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-04-17 12:54:53 -0400

    Add post message functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const messages = {
+  messages: [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date(),
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date(),
+    },
+    {
+      text: "Howdy!",
+      user: "Cowboy Dan",
+      added: new Date(),
+    },
+    {
+      text: "Howdy!",
+      user: "Cowboy Dan",
+      added: new Date(),
+    },
+  ],
+};
+
+router.get("/", function (req, res, next) {
+  res.render("index", messages);
 });
 
 module.exports = router;
+
+router.get("/new", (req, res) => {
+  res.render("form", { title: "Mini Messageboard" });
+});
+
+router.post("/new", (req, res) => {
+  const { user, text } = req.body;
+  messages.messages.push({ user: user, text: text, added: new Date() });
+
+  res.redirect("/");
+});
+
+module.exports = router;
+
+/* GET home page. */
